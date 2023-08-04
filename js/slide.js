2509682a65af326f274db3c91c3002f493721295
unknown <dltjddbs8658@naver.com>
2023-08-04 14:42:27 +0900

    test13

--- js/slide.js
+++ js/slide.js
@@ -8,39 +8,8 @@ const swiperEl2 = document.querySelector(".swiper-2");
 const prevBtnEl3 = document.querySelector(".swiperPrevBtn3");
 const nextBtnEl3 = document.querySelector(".swiperNextBtn3");
 const swiperEl3 = document.querySelector(".swiper-3");
-// const prevBtnEl4 = document.querySelector(".swiperPrevBtn4");
-// const nextBtnEl4 = document.querySelector(".swiperNextBtn4");
-// const swiperEl4 = document.querySelector(".swiper-4");
 
-nextBtnEl1.addEventListener("click", () => {
-  swiperEl.swiper.slideNext();
-});
-prevBtnEl1.addEventListener("click", () => {
-  swiperEl.swiper.slidePrev();
-});
-
-nextBtnEl2.addEventListener("click", () => {
-  swiperEl2.swiper.slideNext();
-});
-prevBtnEl2.addEventListener("click", () => {
-  swiperEl2.swiper.slidePrev();
-});
-
-nextBtnEl3.addEventListener("click", () => {
-  swiperEl3.swiper.slideNext();
-});
-prevBtnEl3.addEventListener("click", () => {
-  swiperEl3.swiper.slidePrev();
-});
-
-// nextBtnEl4.addEventListener("click", () => {
-//   swiperEl4.swiper.slideNext();
-// });
-// prevBtnEl4.addEventListener("click", () => {
-//   swiperEl4.swiper.slidePrev();
-// });
-
-// 화면 size 별 slide 수 설정
+// slide 설정
 const swiperParams = {
   slidesPerView: 1.5,
   injectStylesUrls: ["../style/index.css"],
@@ -61,14 +30,21 @@ const swiperParams = {
     },
   },
 };
+const slideEventFnc = (swiperEl, nextBtn, prevBtn) => {
+  nextBtn.addEventListener("click", () => {
+    swiperEl.swiper.slideNext();
+  });
+  prevBtn.addEventListener("click", () => {
+    swiperEl.swiper.slidePrev();
+  });
 
-// now we need to assign all parameters to Swiper element
-Object.assign(swiperEl, swiperParams);
-Object.assign(swiperEl2, swiperParams);
-Object.assign(swiperEl3, swiperParams);
-// Object.assign(swiperEl4, swiperParams);
-// and now initialize it
-swiperEl.initialize();
-swiperEl2.initialize();
-swiperEl3.initialize();
-// swiperEl4.initialize();
+  // now we need to assign all parameters to Swiper element
+  Object.assign(swiperEl, swiperParams);
+
+  // and now initialize it
+  swiperEl.initialize();
+};
+
+slideEventFnc(swiperEl, nextBtnEl1, prevBtnEl1);
+slideEventFnc(swiperEl2, nextBtnEl2, prevBtnEl2);
+slideEventFnc(swiperEl3, nextBtnEl3, prevBtnEl3);
---
+++ js/test.js
@@ -0,0 +1,6 @@
+const textArea = document.querySelector(".searchBar");
+const inputInitBtn = document.querySelector(".inputImg3");
+
+inputInitBtn.addEventListener("click", () => {
+    textArea.value = "";
+})
