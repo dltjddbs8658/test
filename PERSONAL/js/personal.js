56599ecd2f73e49f6982456ddffafa0238a5ab3b
lsy8658 <socszz@gmail.com>
2024-02-06 16:23:36 +0900

    test

---
+++ PERSONAL/js/personal.js
@@ -0,0 +1,23 @@
+const headerBlurBox = document.querySelector(".blurBox");
+
+let scrolling;
+
+window.addEventListener("scroll", () => {
+  if (!scrolling) {
+    headerBlurBox.style.display = "block";
+  }
+  clearTimeout(scrolling);
+  scrolling = setTimeout(() => {
+    headerBlurBox.style.display = "none";
+    scrolling = undefined;
+  }, 400);
+});
+
+// 일정 높이 이상일 때 blurbox
+// const personalContainer = document.querySelector(".personalContainer");
+// const top = Math.abs(personalContainer.getBoundingClientRect().top);
+// if (top >= 22) {
+// headerBlurBox.style.opacity = 1;
+// } else {
+// headerBlurBox.style.opacity = 0;
+// }
